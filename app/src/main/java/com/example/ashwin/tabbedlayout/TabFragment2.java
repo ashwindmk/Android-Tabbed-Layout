8b5834536d17d81d83f316f02b46703e02db0afd
Ashwin Dinesh <ashwin.dinesh@mirraw.com>
2016-09-15 12:12:04 +0530

    Created Android Tabbed Layout project

---
+++ app/src/main/java/com/example/ashwin/tabbedlayout/TabFragment2.java
@@ -0,0 +1,19 @@
+package com.example.ashwin.tabbedlayout;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ashwin on 4/8/16.
+ */
+public class TabFragment2 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_2, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/example/ashwin/tabbedlayout/TabFragment3.java
@@ -0,0 +1,22 @@
+package com.example.ashwin.tabbedlayout;
+
+/**
+ * Created by ashwin on 4/8/16.
+ */
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by ashwin on 4/8/16.
+ */
+public class TabFragment3 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.tab_fragment_3, container, false);
+    }
+
+}
